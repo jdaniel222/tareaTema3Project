2e182cc411252225b617dd8745257ac55566a730
ubuntu <7949295@alu.murciaeduca.es>
2022-03-04 13:01:57 +0100

    creacion de formulario y creación de modelo db

--- app/private/forms.py
+++ app/private/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileRequired, FileAllowed
-from wtforms import StringField, FileField
+from wtforms import StringField, FileField, PasswordField
 from wtforms.validators import data_required, length, ValidationError, DataRequired, Length
 
 
@@ -10,6 +10,7 @@ class FiltroCl(FlaskForm):
         Length(max=10, message="La longitud del DNI no puede superar los 10 caracteres")
     ])
 
+
 class FormWTF(FlaskForm):
     dni = StringField(label="dni", validators=[
         DataRequired(message="El campo es obligatorio"),
@@ -17,12 +18,12 @@ class FormWTF(FlaskForm):
     ])
     nombre = StringField(label="Nombre", validators=[
         DataRequired(message="El campo es obligatorio"),
-        Length(min = 5, max = 20, message="El campo debe ser entre 5 y 20 caracteres")
+        Length(min=5, max=20, message="El campo debe ser entre 5 y 20 caracteres")
     ])
 
     apellidos = StringField(label="apellidos", validators=[
         DataRequired(message="El campo es obligatorio"),
-        Length(min = 5, max = 50, message="El campo debe ser entre 5 y 50 caracteres")
+        Length(min=5, max=50, message="El campo debe ser entre 5 y 50 caracteres")
     ])
     imagen = FileField(label="imagen", validators=[
         DataRequired(message="El campo es obligatorio"),
@@ -33,3 +34,50 @@ class FormWTF(FlaskForm):
     #     max_length = 1024*1024
     #     if len(field.data.read()) > max_length:
     #         raise ValidationError(f"El fichero no puede ser superior a {max_length}")
+
+class FormLogin(FlaskForm):
+    username = StringField(label="Nombre de usuario", validators=[
+        DataRequired(message="El nombre de usuario es obligatorio"),
+        Length(max=20, message="El nombre de usuario no puede ser superior a 20 caracteres")
+    ])
+    password = PasswordField(label="Contraseña", validators=[
+        DataRequired(message="La contraseña es obligatoria"),
+        Length(min=8, message="La contraseña no puede ser inferior a 8 caracteres")
+    ])
+
+class FormRegistro(FlaskForm):
+    username = StringField(label="Nombre de usuario", validators=[
+        DataRequired(message="El nombre de usuario es obligatorio"),
+        Length(max=15, message="El nombre de usuario no puede ser superior a 15 caracteres")
+    ])
+    password = PasswordField(label="Contraseña", validators=[
+        DataRequired(message="La contraseña es obligatoria"),
+        Length(min=8, message="La contraseña no puede ser inferior a 8 caracteres")
+    ])
+    passwordRepeat = PasswordField(label="Repita la contraseña", validators=[
+        DataRequired(message="contraseña es obligatoria"),
+        Length(min=8, message="La contraseña no puede ser inferior a 8 caracteres")
+    ])
+    dni = StringField(label="DNI", validators=[
+        DataRequired(message="El DNI es obligatorio"),
+        Length(max=10, message="El DNI no puede ser superior a 10 caracteres")
+    ])
+    nombre = StringField(label="Nombre", validators=[
+        DataRequired(message="El nombre es obligatorio"),
+        Length(max=10, message="El nombre no puede ser superior a 20 caracteres")
+    ])
+    apellidos = StringField(label="Apellidos", validators=[
+        DataRequired(message="El nombre es obligatorio"),
+        Length(max=50, message="El nombre no puede ser superior a 50 caracteres")
+    ])
+    # submit = SubmitField(label="Dar de alta")
+
+    def validate_password(form,field):
+        if (field.data).isdigit():
+            raise ValidationError("La contraseña no pueden ser solo digitos")
+        if field.data != form.passwordRepeat.data:
+            raise ValidationError("Las contraseñas no coinciden")
+
+    def validate_passwordRepeat(form, field):
+        if field.data != form.password.data:
+            raise ValidationError("Las contraseñas no coinciden")
--- app/private/models.py
+++ app/private/models.py
@@ -1,3 +1,5 @@
+from werkzeug.security import generate_password_hash, check_password_hash
+
 from app import db
 
 
@@ -10,3 +12,40 @@ class Cliente(db.Model):
     def crearCliente(self):
         db.session.add(self)
         db.session.commit()
+
+class Usuario(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(15), unique=True, nullable=False)
+    password = db.Column(db.String(),nullable=False)
+    nombre = db.Column(db.String(20), nullable=False)
+    apellidos = db.Column(db.String(50), nullable=False)
+
+    def __str__(self):
+        return f"{Usuario.nombre} {Usuario.apellidos}"
+
+    def create(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        try:
+            db.session.commit()
+        except:
+            raise
+    @staticmethod
+    def get_by_id(id):
+        return Usuario.query.get(id)
+
+    @staticmethod
+    def get_by_username(username):
+        return Usuario.query.get(username)
+
+    def set_password(self, passowrd):
+        method = "pbkdf2:sh256:260000"
+        self.password = generate_password_hash(passowrd,method=method)
+
+    def check_password(self, password):
+        if check_password_hash(self.password, password):
+            return True
+        else:
+            return False
